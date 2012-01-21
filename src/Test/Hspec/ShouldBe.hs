db2b21b87b8e698ba0470b21571c0c976c82e067
Simon Hengel <sol@typeful.net>
2012-01-21 17:46:16 +0100

    Depend on test-util

--- src/Test/Hspec/ShouldBe.hs
+++ src/Test/Hspec/ShouldBe.hs
@@ -1,5 +1,6 @@
+{-# LANGUAGE NoImplicitPrelude #-}
 module Test.Hspec.ShouldBe (
-  shouldBe
+  module Test.HUnit.ShouldBe
 , module Test.Hspec.Monadic
 , prop
 , property
@@ -9,8 +10,4 @@ where
 import           Test.Hspec.Monadic
 import           Test.Hspec.QuickCheck
 import           Test.Hspec.HUnit ()
-import           Test.HUnit
-import           Control.Monad
-
-shouldBe :: (Show a, Eq a) => a -> a -> Assertion
-actual `shouldBe` expected = actual @?= expected
+import           Test.HUnit.ShouldBe
