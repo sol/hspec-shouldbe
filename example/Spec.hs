f4a2519595203707599feb06fc46aaf0eb701405
Simon Hengel <sol@typeful.net>
2015-04-23 14:48:30 +0800

    Update example

--- example/Spec.hs
+++ example/Spec.hs
@@ -1,16 +1,9 @@
-module Spec (main, spec) where
+module Spec (spec) where
 
-import           Test.Hspec.ShouldBe
-
-main :: IO ()
-main = hspec spec
+import           Test.Hspec
 
 spec :: Spec
 spec = do
-
   describe "reverse" $ do
     it "reverses a list" $ do
       reverse [1 :: Int, 2, 3] `shouldBe` [3, 2, 1]
-
-    it "gives the original list, if applied twice" $ property $
-      \xs -> reverse (reverse xs) == (xs :: [Int])
